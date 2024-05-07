fb1bfd9288049c09c6ca31afb942d21ef9df73ce
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2024-05-06 20:37:36 -0700

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<div class="flex flex-col items-start gap-3 p-6 z-1">
+	<h1>it's joever</h1>
+	<p>bottom text</p>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: #121212;
+		color: #ffffff;
+	}
+</style>
