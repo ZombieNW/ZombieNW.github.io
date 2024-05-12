83dd07afe1fca47c4a662f95161d1540992406cc
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2024-05-11 19:39:41 -0700

    Update Main Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,86 +11,89 @@
 		<h1 class="text-9xl font-black text-outline">ZombieNW</h1>
 		<h1 class="text-4xl leading-normal font-bold text-outline">Programmer, YouTuber, Mediocre Joke Maker</h1>
 	</div>
-	<img src="./images/znw_logo.png" alt="ZombieNW Logo - Start with a sword in it." class="znw_logo absolute right-24 top-1/3 -translate-y-1/2 w-[20vw]" />
-	<div class="socials">
-		<a href="https://discord.gg/tKFMy85"><i style="color:white;" class="fab fa-discord fa-2x"></i></a>
-		<a href="https://youtube.com/zombienw"><i style="color:white;" class="fab fa-youtube fa-2x"></i></a>
-		<a href="https://twitter.com/zombie_nw"><i style="color:white;" class="fab fa-twitter fa-2x"></i></a>
-		<a href="https://www.twitch.tv/zombie_nw"><i style="color:white;" class="fab fa-twitch fa-2x"></i></a>
-		<a href="https://github.com/ZombieNW"><i style="color:white;" class="fab fa-github fa-2x"></i></a>
-	</div>
+	<img src="./assets/logo.png" alt="ZombieNW Logo - Start with a sword in it." class="znw_logo absolute right-24 top-1/3 -translate-y-1/2 w-[20vw]" />
 </div>
 
-<div class="bg-bg_b_t w-full h-16 text-center flex items-center justify-center border-2 border-border_t hover:border-border_t_h_l">
-	<a href="https://www.youtube.com/@ZombieNW" class="mx-32 text-3xl font-bold hover:italic">Watch <i class="fa-brands fa-youtube"></i></a>
-	<a href="https://store.zombienw.com/" class="mx-32 text-3xl font-bold hover:italic">Shop <i class="fa-solid fa-bag-shopping"></i></a>
-	<a href="/contact" class="mx-32 text-3xl font-bold hover:italic">Contact <i class="fa-solid fa-address-book"></i></a>
-</div>
-
-<h1 class="pl-24 pt-8 text-7xl font-bold text-outline">The Projects</h1>
-
-<div class="grid grid-cols-3 mx-24 my-8 text-shadow">
-	<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#46a8394f]">
-		<p class="text-4xl font-bold">Retro Tennis</p>
-		<p class="-mt-3 text-l">Wii Homebrew Pong Clones</p>
-		<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
+<body class="gradient-bg">
+	<div class="bg-bg_b_t w-full h-16 text-center flex items-center justify-center border-2 border-border_t hover:border-border_t_h_l">
+		<a href="https://www.youtube.com/@ZombieNW" class="mx-32 text-3xl font-bold hover:italic">Watch <i class="fa-brands fa-youtube"></i></a>
+		<a href="https://store.zombienw.com/" class="mx-32 text-3xl font-bold hover:italic">Shop <i class="fa-solid fa-bag-shopping"></i></a>
+		<a href="/contact" class="mx-32 text-3xl font-bold hover:italic">Contact <i class="fa-solid fa-address-book"></i></a>
 	</div>
-	<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#832dd44f]">
-		<p class="text-4xl font-bold">Smplr Chess</p>
-		<p class="-mt-3 text-l">A decluttered chess app for Android.</p>
-		<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
-	</div>
-	<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#1c5a974f]">
-		<p class="text-4xl font-bold">Embedabbl</p>
-		<p class="-mt-3 text-l">Easy to use media wrapper.</p>
-		<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
-	</div>
-	<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#af21664f]">
-		<p class="text-4xl font-bold">ArchOpen</p>
-		<p class="-mt-3 text-l">File association manager for RetroArch on Windows.</p>
-		<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
-	</div>
-	<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#0000004F]">
-		<p class="text-4xl font-bold">Coming Soon</p>
-		<p class="-mt-3 text-l">bottom text</p>
-		<img src="/images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
-	</div>
-	<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#32b9904F]">
-		<p class="text-4xl font-bold">Project Bismuth</p>
-		<p class="-mt-3 text-l">A discontinued FPS game for Windows.</p>
-		<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
-	</div>
-</div>
-<hr class="w-1/4 mx-auto" />
 
-<h1 class="pl-24 pt-8 text-7xl font-bold text-outline">The "Me"</h1>
+	<h1 class="pl-24 pt-8 text-7xl font-bold text-outline">The Projects</h1>
 
-<div class="w-full flex items-center justify-center">
-	<img src="/images/me.png" class="profile" alt="ZombieNW's avatar, also known as a Blob" />
-	<div class="w-1/2">
-		<h1 class="text-6xl font-black text-outline">Hi, it's me, ZombieNW!</h1>
-		<p class="text-2xl indent-8 mt-2">
-			I'm an amateur developer and YouTuber who likes Node.JS, Svelte, and Tailwind. I also have experience in other languages such as Java and GDScript. I'm always learning and working on something new. I also enjoy making YouTube videos about my
-			creations and things I find interesting. Want to work together? <a href="/contact" class="italic">Contact me!</a>
-		</p>
+	<div class="grid grid-cols-3 mx-24 my-8 text-shadow">
+		<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#46a8394f]">
+			<p class="text-4xl font-bold">Retro Tennis</p>
+			<p class="-mt-3 text-l">Wii Homebrew Pong Clones</p>
+			<img src="./assets/retro/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
+		</div>
+		<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#832dd44f]">
+			<p class="text-4xl font-bold">Smplr Chess</p>
+			<p class="-mt-3 text-l">A decluttered chess app for Android.</p>
+			<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
+		</div>
+		<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#1c5a974f]">
+			<p class="text-4xl font-bold">Embedabbl</p>
+			<p class="-mt-3 text-l">Easy to use media wrapper.</p>
+			<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
+		</div>
+		<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#af21664f]">
+			<p class="text-4xl font-bold">ArchOpen</p>
+			<p class="-mt-3 text-l">File association manager for RetroArch on Windows.</p>
+			<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
+		</div>
+		<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#0000004F]">
+			<p class="text-4xl font-bold">Coming Soon</p>
+			<p class="-mt-3 text-l">bottom text</p>
+			<img src="/images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
+		</div>
+		<div class="flex project-card flex-col items-start gap-3 p-5 m-6 z-1 h-64 shadow-lg border-2 border-border_t hover:border-border_t_h rounded-xl bg-[#32b9904F]">
+			<p class="text-4xl font-bold">Project Bismuth</p>
+			<p class="-mt-3 text-l">A discontinued FPS game for Windows.</p>
+			<img src="./images/retrotennis_logo.png" alt="Retro Tennis Logo" class="overflow-hidden mx-auto" />
+		</div>
 	</div>
-</div>
+	<hr class="w-1/4 mx-auto" />
 
-<center>
-	<h1 class="text-xl my-4">Can't Find Something? Try our <a href="/lost-and-found" class="italic">Lost & Found</a></h1>
-</center>
+	<h1 class="pl-24 pt-8 text-7xl font-bold text-outline">The "Me"</h1>
 
-<hr class="w-1/4 mx-auto" />
+	<div class="w-full flex items-center justify-center">
+		<img src="/assets/index/profile.png" class="profile" alt="ZombieNW's avatar, also known as a Blob" />
+		<div class="w-1/2">
+			<h1 class="text-6xl font-black text-outline">Hi, it's me, ZombieNW!</h1>
+			<p class="text-2xl indent-8 mt-2">
+				I'm an amateur developer and YouTuber who likes Node.JS, Svelte, and Tailwind. I also have experience in other languages such as Java and GDScript. I'm always learning and working on something new. I also enjoy making YouTube videos about my
+				creations and things I find interesting. Want to work together? <a href="/contact" class="italic">Contact me!</a>
+			</p>
+		</div>
+	</div>
+	<hr class="w-1/4 mx-auto" />
+	<div class="w-full flex items-center justify-center my-8">
+		<a href="https://discord.gg/tKFMy85"><i class="fab fa-discord fa-2x m-4 text-5xl"></i></a>
+		<a href="https://youtube.com/zombienw"><i class="fab fa-youtube fa-2x m-4 text-5xl"></i></a>
+		<a href="https://twitter.com/zombie_nw"><i class="fab fa-twitter fa-2x m-4 text-5xl"></i></a>
+		<a href="https://www.twitch.tv/zombie_nw"><i class="fab fa-twitch fa-2x m-4 text-5xl"></i></a>
+		<a href="https://github.com/ZombieNW"><i class="fab fa-github fa-2x m-4 text-5xl"></i></a>
+	</div>
 
-<center>
-	<h1 class="text-xl my-4">ZombieNW, {new Date().getFullYear()}</h1>
-</center>
+	<hr class="w-1/4 mx-auto" />
+
+	<center>
+		<h1 class="text-xl my-4">Can't Find Something? Try our <a href="/lost-and-found" class="italic">Lost & Found</a></h1>
+	</center>
+
+	<hr class="w-1/4 mx-auto" />
+
+	<center>
+		<h1 class="text-xl pt-6 pb-8">ZombieNW, {new Date().getFullYear()}</h1>
+	</center>
+</body>
 
 <style lang="postcss">
 	:global(html) {
 		background-color: #121212;
-		background: rgb(23, 23, 23);
-		background: linear-gradient(180deg, rgba(23, 23, 23, 1) 0%, rgba(5, 2, 7, 1) 100%);
 		color: #ffffff;
 		font-family: 'Rubik', sans-serif;
 		font-optical-sizing: auto;
@@ -99,7 +102,7 @@
 	}
 
 	.hero {
-		background-image: url('/images/space.jpg');
+		background-image: url('/assets/index/top-container-bg.jpg');
 	}
 
 	.profile,
