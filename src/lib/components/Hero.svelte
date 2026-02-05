9f886d867fe29d206358f5b7223f45cf0ef46c15
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-02-05 13:51:09 -0700

    Update Hero Z Ordering

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -2,23 +2,36 @@
 	import Space from '$lib/components/Space.svelte';
 </script>
 
-<div class="h-[70vh] bg-linear-to-b from-purple-900 to-gray-950">
-	<div class="w-full h-full flex md:flex-row flex-col md:mx-12 items-center justify-center">
-		<div class="md:text-left text-center md:h-2/3">
-			<h2 class="md:text-[6rem] text-3xl font-black text-outline leading-20">Hi, It's Me,</h2>
-			<h1 class="md:text-[10rem] text-5xl font-black text-outline">ZombieNW</h1>
-			<h3 class="md:text-[2.5rem] text-2xl leading-4 font-bold text-outline">
+<div class="relative h-[70vh] w-full overflow-hidden bg-gray-950">
+	<div class="absolute inset-0 z-0">
+		<div
+			class="absolute inset-0 bg-linear-to-b from-purple-800/40 via-purple-950/40 to-gray-950"
+		></div>
+
+		<Space backgroundColor="transparent" class="h-full w-full opacity-80" />
+
+		<div class="absolute inset-0 bg-linear-to-t from-gray-950 via-transparent to-transparent"></div>
+	</div>
+
+	<div
+		class="relative z-10 flex h-full w-full flex-col items-center justify-center md:flex-row md:px-12"
+	>
+		<div class="md:h-2/3 md:text-left text-center">
+			<h2 class="text-3xl font-black text-outline leading-20 md:text-[6rem] drop-shadow-2xl">
+				Hi, It's Me,
+			</h2>
+			<h1 class="text-5xl font-black text-outline md:text-[10rem] drop-shadow-2xl">ZombieNW</h1>
+			<h3 class="text-2xl font-bold text-outline leading-4 md:text-[2.5rem] drop-shadow-lg">
 				Programmer, YouTuber, Mediocre Joke Maker
 			</h3>
 		</div>
-		<div class="flex justify-center md:flex-1 md:h-2/3">
+
+		<div class="flex justify-center md:h-2/3 md:flex-1">
 			<img
 				src="/logo.png"
-				class="aspect-square w-[15vw] h-[15vw]"
+				class="aspect-square h-[15vw] w-[15vw] min-w-[150px] drop-shadow-[0_0_15px_rgba(168,85,247,0.4)]"
 				alt="A yellow star with a blue sword with a brown hilt going through it."
 			/>
 		</div>
 	</div>
-	<Space backgroundColor="#000033" class="inset-0 -translate-y-full" />
-	<div class="bg-linear-to-b from-transparent to-gray-950 h-1/2 -translate-y-[300%]"></div>
 </div>
