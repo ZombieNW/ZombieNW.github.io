e3277c3276f6ce09fc968274cf9fb1fbd8802c8b
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-02-05 22:39:03 -0700

    Further project hero sizing changes

--- src/lib/components/ProjectHero.svelte
+++ src/lib/components/ProjectHero.svelte
@@ -30,7 +30,7 @@
 
 	<div class="relative z-10 flex h-full w-full flex-col items-center justify-center px-4 md:px-12">
 		{#if title}
-			<img src={img} alt="{title} project logo" class="max-h-2/3 object-contain" />
+			<img src={img} alt="{title} project logo" class="max-h-2/3 object-contain md:-mt-24" />
 			<h1 class="text-6xl font-black text-outline md:text-7xl drop-shadow-2xl -mt-12">
 				{title}
 			</h1>
