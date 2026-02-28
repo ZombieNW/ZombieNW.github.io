a1015f9af7ebc065ef7edccd7be0fd9ec1425d45
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-02-28 12:03:42 -0700

    Add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,3 +15,20 @@
 <div class="w-full h-12 text-center mt-8 z-20 relative">
 	Made with ❤️ by ZombieNW {new Date().getFullYear()}
 </div>
+
+<meta property="og:title" content="ZombieNW | zombienw.com" />
+<meta
+	property="og:description"
+	content="Hi! It's me! ZombieNW. I'm a computer science student always working on something."
+/> <meta property="og:type" content="website" />
+<meta property="og:url" content="https://zombienw.com" />
+<meta property="og:image" content="https://zombienw.com/banner.png" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="ZombieNW | zombienw.com" />
+<meta
+	name="twitter:description"
+	content="Hi! It's me! ZombieNW. I'm a computer science student always working on something."
+/> <meta name="twitter:image" content="https://zombienw.com/banner.png" />
+
+<meta name="theme-color" content="#030712" />
