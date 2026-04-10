51b6bfd8e13575a031d629f3ef88639114faec71
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-04-10 08:03:23 -0700

    Thank you, Christian

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,23 @@
 	<link rel="icon" href="/favicon.png" />
 	<title>ZombieNW</title>
 	<base target="_blank" />
+
+	<meta property="og:title" content="ZombieNW | zombienw.com" />
+	<meta
+		property="og:description"
+		content="Hi! It's me! ZombieNW. I'm a computer science student always working on something."
+	/> <meta property="og:type" content="website" />
+	<meta property="og:url" content="https://zombienw.com" />
+	<meta property="og:image" content="https://zombienw.com/banner.png" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="ZombieNW | zombienw.com" />
+	<meta
+		name="twitter:description"
+		content="Hi! It's me! ZombieNW. I'm a computer science student always working on something."
+	/> <meta name="twitter:image" content="https://zombienw.com/banner.png" />
+
+	<meta name="theme-color" content="#030712" />
 </svelte:head>
 
 {@render children()}
@@ -15,20 +32,3 @@
 <div class="w-full h-12 text-center mt-8 z-20 relative">
 	Made with ❤️ by ZombieNW {new Date().getFullYear()}
 </div>
-
-<meta property="og:title" content="ZombieNW | zombienw.com" />
-<meta
-	property="og:description"
-	content="Hi! It's me! ZombieNW. I'm a computer science student always working on something."
-/> <meta property="og:type" content="website" />
-<meta property="og:url" content="https://zombienw.com" />
-<meta property="og:image" content="https://zombienw.com/banner.png" />
-
-<meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:title" content="ZombieNW | zombienw.com" />
-<meta
-	name="twitter:description"
-	content="Hi! It's me! ZombieNW. I'm a computer science student always working on something."
-/> <meta name="twitter:image" content="https://zombienw.com/banner.png" />
-
-<meta name="theme-color" content="#030712" />
